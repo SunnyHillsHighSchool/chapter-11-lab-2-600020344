dd7bbb90b7720870de1d723d9d057df50d2bff1c
600020344 <77069279+600020344@users.noreply.github.com>
2021-01-27 01:56:14 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
     //add test cases
+    int[] testCases = new int[]{496,45,6,14,8128,1245,33,28,27,33550336};
+    for (int i = 0; i<testCases.length; i++)
+    {
+      String str = "";
+      if (Perfect.isPerfect(testCases[i]))
+        str = " is perfect.";
+      else
+        str = " is not perfect.";
+      System.out.println(testCases[i] + str);
+    }
   }
 }
