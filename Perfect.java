6434addc33d05aff02cd85c6e6735b7d82203836
600020344 <77069279+600020344@users.noreply.github.com>
2021-01-27 01:57:01 -0800

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -6,6 +6,14 @@ public class Perfect
 {
 	public static boolean isPerfect( int number )
 	{
+    int sum = 0;
+    for (int i = 1; i<number; i++)
+    {
+      if (number%i==0)
+        sum+=i;
+    }
+    if (sum==number)
+      return true;
 		return false;
 	}
 }
